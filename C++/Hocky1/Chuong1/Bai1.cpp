f847adf0969e6453b57384a10a0efd8ea7554a50
Nguyen12345tt <thanhnguyennguyen496@gmail.com>
2025-07-21 23:13:51 +0700

    Tính tổng

---
+++ C++/Hocky1/Chuong1/Bai1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //Lưu số nguyên trong các biến
+    int a=50;
+    int b=100;
+    // Tính tổng và lưu trong biến total
+    int tong=a+b;
+    //In ra kết quả
+    cout<<"Tong cua a va b la: "<<tong<<endl;
+}
