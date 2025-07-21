4ccfb1308676afe740e0e55b280f8c0961359782
Nguyen12345tt <thanhnguyennguyen496@gmail.com>
2025-07-22 00:32:29 +0700

    Đổi độ C sang độ F

---
+++ C++/Hocky1/Chuong2/Bai12.cpp
@@ -0,0 +1,17 @@
+//Trịnh Anh Tuấn
+//Mã sinh viên:1871020635
+//Lớp: CNTT 18-09
+
+#include <iostream>
+using namespace std;
+int main() 
+{
+    float c, f;
+
+    cout << "Nhập nhiệt độ (°C): ";
+    cin >> c;
+
+    f = (9.0 / 5.0) * c + 32;
+    cout << "Nhiệt độ tương ứng (°F): " << f << endl;
+
+}
