64fd0b51cd0ac48f772a7442bee53a8211ff4e0a
Nguyen12345tt <thanhnguyennguyen496@gmail.com>
2025-07-22 00:22:01 +0700

    Số dòng tam giác

---
+++ C++/Hocky1/Chuong1/Bai15.cpp
@@ -0,0 +1,19 @@
+//Trịnh Anh Tuấn
+//Mã sinh viên:1871020635
+//Lớp: CNTT 18-09
+
+#include <iostream>
+using namespace std;
+int main() {
+    int dong = 5; // Số dòng của tam giác
+
+    for (int i = 1; i <= dong; i++) {
+        for (int j = 1; j <= dong - i; j++) {
+            cout << " "; // In khoảng trắng
+        }
+        for (int k = 1; k <= 2 * i - 1; k++) {
+            cout << "*"; // In dấu sao
+        }
+        cout << endl; // Xuống dòng
+    }
+}
